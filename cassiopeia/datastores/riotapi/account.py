86c62322b2f70b79d0a7ac6d90ecf9676c8d76f8
stephen19k <25879872+stephen19K@users.noreply.github.com>
2024-04-25 15:27:47 +0800

    copied wrong file

--- cassiopeia/datastores/riotapi/account.py
+++ cassiopeia/datastores/riotapi/account.py
@@ -1,161 +1,82 @@
-from typing import Union
+from typing import Type, TypeVar, MutableMapping, Any, Iterable
+import urllib
 
-from datapipelines import NotFoundError
-from merakicommons.cache import lazy_property
-from merakicommons.container import searchable
-
-from ..data import Region
-from .common import CoreData, CassiopeiaGhost, ghost_load_on
-from ..dto.account import AccountDto
-
-##############
-# Data Types #
-##############
-
-
-class AccountData(CoreData):
-    _dto_type = AccountDto
-    _renamed = {}
-
-##############
-# Core Types #
-##############
-
-
-@searchable(
-    {
-        str: ["puuid", "gameName", "tagLine", ]
-    }
+from datapipelines import (
+    DataSource,
+    PipelineContext,
+    Query,
+    NotFoundError,
+    validate_query,
 )
-class Account(CassiopeiaGhost):
-    _data_types = {AccountData}
+from .common import RiotAPIService, APINotFoundError
+from ...data import Region, Platform
+from ...dto.account import AccountDto
+from ..uniquekeys import convert_to_continent
 
-    def __init__(
+T = TypeVar("T")
+
+
+class AccountAPI(RiotAPIService):
+    @DataSource.dispatch
+    def get(
             self,
-            *,
-            puuid: str = None,
-            gameName: str = None,
-            tagLine: str = None,
-            region: Union[Region, str] = None
-    ):
-        kwargs = {"region": region}
+            type: Type[T],
+            query: MutableMapping[str, Any],
+            context: PipelineContext = None,
+    ) -> T:
+        pass
 
-        if puuid is not None:
-            kwargs["puuid"] = puuid
-        if gameName is not None:
-            kwargs["gameName"] = gameName
-        if tagLine is not None:
-            kwargs["tagLine"] = tagLine
-        super().__init__(**kwargs)
+    @DataSource.dispatch
+    def get_many(
+            self,
+            type: Type[T],
+            query: MutableMapping[str, Any],
+            context: PipelineContext = None,
+    ) -> Iterable[T]:
+        pass
 
-    @classmethod
-    def __get_query_from_kwargs__(
-            cls,
-            *,
-            puuid: str = None,
-            gameName: str = None,
-            tagLine: str = None,
-            region: Union[Region, str]
-    ) -> dict:
-        query = {"region": region}
+    _validate_get_account_query = (
+        Query.has("puuid")
+        .as_(str)
+        .or_("gameName")
+        .as_(str)
+        .and_("tagLine")
+        .as_(str)
+        .also.has("region")
+        .as_(Region)
+    )
 
-        if puuid is not None:
-            query["puuid"] = puuid
-        if gameName is not None:
-            query["gameName"] = gameName
-        if tagLine is not None:
-            query["tagLine"] = tagLine
-
-        return query
-
-    def __get_query__(self):
-        query = {"region": self.region, "platform": self.platform}
-
-        try:
-            query["puuid"] = self._data[AccountData].puuid
-        except AttributeError:
-            pass
-        try:
-            query["gameName"] = self._data[AccountData].gameName
-        except AttributeError:
-            pass
-        try:
-            query["tagLine"] = self._data[AccountData].tagLine
-        except AttributeError:
-            pass
-        assert (
-                ("name" in query and "tagLine" in query) or "puuid" in query
-        )
-        return query
-
-    def __eq__(self, other: "Account"):
-        if not isinstance(other, Account) or self.region != other.region:
-            return False
-        s = {}
-        o = {}
-        if hasattr(self._data[AccountData], "puuid"):
-            s["puuid"] = self.puuid
-        if hasattr(other._data[AccountData], "puuid"):
-            o["puuid"] = other.puuid
-        if hasattr(self._data[AccountData], "gameName"):
-            s["gameName"] = self.gameName
-        if hasattr(other._data[AccountData], "gameName"):
-            o["gameName"] = other.gameName
-        if hasattr(self._data[AccountData], "tagLine"):
-            s["tagLine"] = self.tagLine
-        if hasattr(other._data[AccountData], "tagLine"):
-            o["tagLine"] = other.tagLine
-        if any(s.get(key, "s") == o.get(key, "o") for key in s):
-            return True
+    @get.register(AccountDto)
+    @validate_query(_validate_get_account_query, convert_to_continent)
+    def get_summoner(
+            self, query: MutableMapping[str, Any], context: PipelineContext = None
+    ) -> AccountDto:
+        if "puuid" in query:
+            url = ("https://{platform}.api.riotgames.com/lol/account/v1/accounts/by-puuid/{"
+                   "puuid}").format(
+                platform=query["continent"].value.lower(), puuid=query["puuid"]
+            )
+            endpoint = "accounts/by-puuid/puuid"
+        elif "gameName" in query and "tagLine" in query:
+            url = ("https://{platform}.api.riotgames.com/lol/account/v1/accounts/by-riot-id/{"
+                   "gameName}/{tagLine}").format(
+                platform=query["continent"].value.lower(),
+                gameName=query["gameName"],
+                tagLine=query["tagLine"]
+            )
+            endpoint = "accounts/by-riot-id/gameName/tagLine"
         else:
-            return self.puuid == other.puuid
+            endpoint = ""
 
-    def __str__(self):
-        tagLine = "?"
-        gameName = "?"
-        puuid = "?"
-
-        if hasattr(self._data[AccountData], "tagLine"):
-            tagLine = self.tagLine
-        if hasattr(self._data[AccountData], "gameName"):
-            gameName = self.gameName
-        if hasattr(self._data[AccountData], "puuid"):
-            puuid = self.puuid
-
-        return "Account(gameName='{gameName}', tagLine='{tagLine}' puuid='{puuid}')".format(
-            gameName=gameName, tagLine=tagLine, puuid=puuid
-        )
-
-    @property
-    def exists(self):
         try:
-            if not self._Ghost__all_loaded:
-                self.__load__()
-            self.puuid  # Make sure we can access this attribute
-            return True
-        except (AttributeError, NotFoundError):
-            return False
+            app_limiter, method_limiter = self._get_rate_limiter(
+                Platform.from_region(query["region"]), endpoint
+            )
+            data = self._get(
+                url, {}, app_limiter=app_limiter, method_limiter=method_limiter
+            )
+        except APINotFoundError as error:
+            raise NotFoundError(str(error)) from error
 
-    @lazy_property
-    def region(self) -> Region:
-        """The region for this summoner."""
-        return Region(self._data[AccountData].region)
-
-    @CassiopeiaGhost.property(AccountData)
-    @ghost_load_on
-    def puuid(self) -> str:
-        return self._data[AccountData].puuid
-
-    @CassiopeiaGhost.property(AccountData)
-    @ghost_load_on
-    def gameName(self) -> str:
-        return self._data[AccountData].gameName
-
-    @CassiopeiaGhost.property(AccountData)
-    @ghost_load_on
-    def tagLine(self) -> str:
-        return self._data[AccountData].tagLine
-
-    @property
-    def riot_id(self) -> str:
-        return self.gameName + "#" + self.tagLine
+        data["region"] = query["continent"].region.value
+        return AccountDto(**data)
